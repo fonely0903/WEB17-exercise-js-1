f9b31331825aa457d7b2635babce93308dd2cb0c
fonely0903 <fonely0903@yahoo.com.tw>
2017-04-25 20:48:17 +0800

    init

---
+++ js/js.js
@@ -0,0 +1,6 @@
+var todoInputEle    = document.getElementById("todo-input") ;
+var submitBtnEle    = document.getElementById("todo-submit") ;
+var allDoneBtnEle   = document.getElementById("todo-allDone");
+
+var todoListEle     = document.getElementById("todo-todoList") ;
+var todoEles 		= document.getElementsByClassName("list-group-item");
