a543f4815a7effd722ab14c8b65c02a085e35e34
fonely0903 <fonely0903@yahoo.com.tw>
2017-04-25 20:50:16 +0800

    Delete js.js

--- js.js
+++
@@ -1,6 +0,0 @@
-var todoInputEle    = document.getElementById("todo-input") ;
-var submitBtnEle    = document.getElementById("todo-submit") ;
-var allDoneBtnEle   = document.getElementById("todo-allDone");
-
-var todoListEle     = document.getElementById("todo-todoList") ;
-var todoEles 		= document.getElementsByClassName("list-group-item");
